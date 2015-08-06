bc75cca0730dd67b0ccc647d30a8cb45124ee21f
kglover29 <kglover29@gmail.com>
2015-08-06 02:40:13 -0700

    Email formData send 0.0.4

--- js/jquery.emailform.js
+++ js/jquery.emailform.js
@@ -1,3 +1,7 @@
-$('#fileupload').fileupload({
-    formData: {example: 'test'}
+$('#email').bind('fileuploadsubmit', function (e, data) {
+    var input = $('#input');
+    data.formData = {}
+              dataType: 'json',
+     sequentialUploads: true,
+              formData: {email: input.val()};
 });
